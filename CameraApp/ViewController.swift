02745dbc10ad3d8d6ad5d54bb0638f81baff40f1
taiga <matsuotaiga.miraidenshi@gmail.com>
2019-07-06 01:20:07 +0900

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by 松尾大雅 on 2019/07/06.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self .present(picker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+     let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+            }
+        
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
